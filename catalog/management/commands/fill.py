09c06b724fd6542cb400d628f49eb835bc341921
Maxim Abavi <afghan1363@ya.ru>
2023-10-28 19:27:11 +0500

    Джанго домашка 3

---
+++ catalog/management/commands/fill.py
@@ -0,0 +1,24 @@
+import os
+
+from django.core.management import BaseCommand
+from django.db import connection
+
+from catalog.models import Category, Product
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        # Очистка таблицы Product
+        Product.objects.all().delete()
+
+        # Очистка таблицы Category
+        Category.objects.all().delete()
+        # # Сброс автоинкремента для поля `pk` в таблице Category
+        # with connection.cursor() as cursor:
+        #     cursor.execute("ALTER SEQUENCE catalog_category_id_seq RESTART WITH 1")
+        #
+        # # Сброс автоинкремента для поля `pk` в таблице Product
+        # with connection.cursor() as cursor:
+        #     cursor.execute("ALTER SEQUENCE catalog_product_id_seq RESTART WITH 1")
+        return os.system("python manage.py loaddata catalog.json")
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(max_length=500, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('title',)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(max_length=500, verbose_name='Описание')
+    image = models.ImageField(upload_to='students/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    date_of_create = models.DateField(verbose_name='Дата создания')
+    date_of_change = models.DateField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.title} - {self.description}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('title',)
