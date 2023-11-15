2092e7018bbcfb084853bb785998c7cc95f821b9
Maxim Abavi <afghan1363@ya.ru>
2023-11-15 21:25:51 +0500

    переопределил регистрацию на поле email

--- users/models.py
+++ users/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from catalog.models import NULLABLE
 
+
 # Create your models here.
 class User(AbstractUser):
+    username = None  # для деактивации главного поля авторизации
     phone = models.CharField(max_length=35, verbose_name='Телефон')
     avatar = models.ImageField(upload_to='users/', verbose_name='Ава', **NULLABLE)
     country = models.CharField(max_length=50, verbose_name='Страна')
+    email = models.EmailField(unique=True, verbose_name='Почта')
 
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
