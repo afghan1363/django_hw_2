9a7a8d992a230d345468d2f221a38257544f99ba
Maxim Abavi <afghan1363@ya.ru>
2023-11-19 05:24:49 +0500

    Восстановление доступа, если забыл пароль; возможность изменения пароля; пароли генерируются

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from users.views import UserRegisterView, ProfileView, verification
+from users.views import (UserRegisterView, ProfileView, verify_mail, UserForgotPasswordView, GenerateNewPassword,
+                         generate_new_password)
 from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'users'
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('reset_password/', UserForgotPasswordView.as_view(), name='reset_password'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('registration/', UserRegisterView.as_view(), name='reg'),
-    path('verification_code/<str:code>/', verification, name='verification_code'),
+    path('verification_code/<str:code>/', verify_mail, name='verification_code'),
     path('profile/edit/', ProfileView.as_view(), name='profile'),
+    path('set_new_password/<uidb64>/<token>/', GenerateNewPassword.as_view(), name='set_new_password'),
+    path('profile/new_pass', generate_new_password, name='new_pass')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, View
 from users.models import User
 from users.forms import UserRegisterForm, UserProfileForm
 from django.urls import reverse_lazy, reverse
@@ -7,7 +7,10 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 from django.core.mail import send_mail
 from config.settings import EMAIL_HOST_USER
+from catalog.forms import StyleFormMixin
 import random
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.views import PasswordResetView
 
 
 # Create your views here.
@@ -27,7 +30,6 @@ class UserRegisterView(CreateView):
         new_user = form.save()
         code = ''.join(random.sample('0123456789', 4))
         new_user.verification_code = code
-        # new_user.is_active = False
         send_mail(
             subject='Верификация SkyStore',
             message=f'''Перейдите по ссылке для верификации: http://127.0.0.1:8000/users/verification_code/{code}
@@ -38,13 +40,14 @@ class UserRegisterView(CreateView):
         return super().form_valid(form)
 
 
-def verification(request, code):
+def verify_mail(request, code):
     """
     Контроллер верификации email
     """
     user = User.objects.get(verification_code=code)
     user.is_active = True
     user.save()
+    messages.success(request, 'Ваш аккаунт активирован!')
     return redirect(reverse('users:login'))
 
 
@@ -63,6 +66,24 @@ class ProfileView(UpdateView):
         return self.request.user
 
 
+class UserForgotPasswordView(SuccessMessageMixin, PasswordResetView):
+    """
+    Представление по сбросу пароля по почте
+    """
+    form_class = PasswordResetForm
+    from_email = EMAIL_HOST_USER
+    template_name = 'users/user_password_reset.html'
+    success_url = reverse_lazy('users:login')
+    success_message = 'Письмо с новым паролем отправлено на ваш email'
+    # subject_template_name = 'Восстановление пароля на сайте SkyStore'
+    email_template_name = 'users/mail_password_reset.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Запрос на восстановление пароля'
+        return context
+
+
 def generate_new_password(request):
     """
     Контроллер генерации нового пароля и отправка его на почту
@@ -71,10 +92,42 @@ def generate_new_password(request):
     request.user.set_password(new_password)
     request.user.save()
     send_mail(
-        subject='Восстановление пароля',
+        subject='Изменение пароля',
         message=f'Ваш новый пароль для авторизации: {new_password}',
         from_email=EMAIL_HOST_USER,
         recipient_list=[request.user.email]
     )
     messages.success(request, 'Вам на почту отправлено письмо с новым паролем для вашего аккаунта')
     return redirect(reverse('users:login'))
+
+
+class GenerateNewPassword(SuccessMessageMixin, View):
+    def get(self, request, uidb64: str, token: str):
+        user = self.get_user(uidb64)
+        from django.contrib.auth.tokens import default_token_generator
+        if not user or not default_token_generator.check_token(user, token):
+            messages.warning(self.request, 'Ошибка подтверждения, попробуйте ещё раз.')
+        else:
+            new_password = User.objects.make_random_password()
+            user.set_password(new_password)
+            user.save()
+            send_mail(
+                subject='Восстановление пароля',
+                message=f'Ваш новый пароль для авторизации: {new_password}',
+                from_email=EMAIL_HOST_USER,
+                recipient_list=[user.email]
+            )
+            messages.success(request, 'Вам на почту отправлено письмо с новым паролем для вашего аккаунта')
+            return redirect(reverse('users:login'))
+
+    @staticmethod
+    def get_user(uid_base64: str) -> User | None:
+        try:
+            from django.utils.http import urlsafe_base64_decode
+            uid = urlsafe_base64_decode(uid_base64).decode()
+            user_id = int(uid)
+            user = User.objects.get(pk=user_id)
+        except (ValueError, User.DoesNotExist):
+            user = None
+        return user
+
